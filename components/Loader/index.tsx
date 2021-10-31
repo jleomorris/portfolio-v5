b5d4814ee725ac18e48a3c3170e3af7e8760e919
jleomorris <james-morris2010@hotmail.co.uk>
2021-10-31 15:59:03 +0000

    Add Loader & Image component

---
+++ components/Loader/index.tsx
@@ -0,0 +1,94 @@
+import React, { useEffect } from 'react';
+// Animation
+import { motion } from 'framer-motion';
+// Components
+import Image from './components/Image';
+import image1 from '../../images/image-1.jpg';
+
+// Variants
+const container = {
+  show: {
+    transition: {
+      staggerChildren: 0.35,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: {
+      ease: 'easeInOut',
+      duration: 0.8,
+    },
+  },
+};
+
+const itemMain = {
+  hidden: {
+    opacity: 0,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+};
+
+const Loader = ({ setLoading }) => {
+  return (
+    <div className='loader'>
+      <motion.div
+        className='loader-inner'
+        variants={container}
+        initial='hidden'
+        animate='show'
+        exit='exit'
+        // onAnimationComplete={() => setLoading(false)}
+      >
+        <ImageBlock variants={item} id='image-1' />
+        <motion.div className='transition-image' variants={itemMain}>
+          <motion.img
+            src={`../../images/image-2.jpg`}
+            alt='random alt'
+            layoutId='main-image-1'
+          />
+        </motion.div>
+        <ImageBlock variants={item} id='image-3' />
+        <ImageBlock variants={item} id='image-4' />
+        <ImageBlock variants={item} id='image-5' />
+      </motion.div>
+    </div>
+  );
+};
+
+export const ImageBlock = ({ id, variants }) => {
+  return (
+    <motion.div className={`image-block ${id}`} variants={variants}>
+      <Image
+        src={`../../images/${id}.webp`}
+        fallback={`../../images/${id}.jpg`}
+        alt={id}
+      />
+    </motion.div>
+  );
+};
+
+export default Loader;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,59 @@
+import { useState, useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
 import '../styles/globals.css';
 // Animation
-import { AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 // Components
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
+import Loader from '../components/Loader';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+      ? document.querySelector('body').classList.add('loading')
+      : document.querySelector('body').classList.remove('loading');
+  }, [loading]);
+
   return (
     <Provider store={store}>
-      <AnimatePresence
-        exitBeforeEnter
-        // initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Nav />
-        <Component {...pageProps} />
-        <Footer />
-      </AnimatePresence>
+      <AnimateSharedLayout type='crossfade'>
+        <AnimatePresence
+          exitBeforeEnter
+          // initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          {loading ? (
+            <motion.div key='loader'>
+              <Loader setLoading={setLoading} />
+            </motion.div>
+          ) : (
+            <>
+              {/* <Header /> */}
+              {/* <Banner /> */}
+              {/* <Nav /> */}
+              <Component {...pageProps} />
+              <Footer />
+              {!loading && (
+                <div className='transition-image final'>
+                  <motion.img
+                    src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
+                    layoutId='main-image-1'
+                    transition={{
+                      ease: [0.6, 0.01, -0.05, 0.95],
+                      duration: 1.6,
+                    }}
+                  />
+                </div>
+              )}
+            </>
+          )}
+        </AnimatePresence>
+      </AnimateSharedLayout>
     </Provider>
   );
 }
