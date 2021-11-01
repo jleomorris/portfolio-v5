0fd84e8ab13d9c53eeeb35da466a7d0be0d3d874
jleomorris <james-morris2010@hotmail.co.uk>
2021-11-01 18:38:07 +0000

    Update PageContainer padding

--- components/PageContainer/index.tsx
+++ components/PageContainer/index.tsx
@@ -2,12 +2,12 @@ import { ReactNode } from 'react';
 
 interface IProps {
   children: ReactNode;
-  bgColor: string;
+  bgColor?: string;
 }
 
 const PageContainer: React.FC<IProps> = ({ children, bgColor }) => {
   return (
-    <div className={`page-container min-h-screen p-10 md:p-20 ${bgColor}`}>
+    <div className={`page-container min-h-screen p-10 md:p-40 ${bgColor}`}>
       {children}
     </div>
   );
