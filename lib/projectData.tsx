8bd679637384f653318b879be35512ed8c746010
jleomorris <james-morris2010@hotmail.co.uk>
2021-11-13 03:34:27 +0000

    Add links, headerTextColors and headerImgURLs to project data

--- lib/projectData.tsx
+++ lib/projectData.tsx
@@ -1,42 +1,62 @@
 export const projectData = [
   {
+    link: 'LDR_Generator',
     subTitle: 'LDR Generator',
     title: 'Some Love Death & Robots Randomness',
     backgroundColor: '#D62742',
     textColor: 'text-white',
-    imgURL:
+    headerTextColor: 'text-white',
+    headerImgURL:
+      'https://res.cloudinary.com/jleomorris/image/upload/v1636763556/Portfolio-v5/Projects/LDR/header_background.jpg',
+    sectionImgURL:
       'https://res.cloudinary.com/jleomorris/image/upload/f_auto,q_auto/v1636320699/Portfolio-v5/Projects/kharnivore.jpg',
   },
   {
+    link: 'Poke_Price',
     subTitle: 'Poke Price',
     title: 'Get the prices of your childhood favourites',
     backgroundColor: '#dfc684',
     textColor: 'text-black',
-    imgURL:
+    headerTextColor: 'text-white',
+    headerImgURL:
+      'https://res.cloudinary.com/jleomorris/image/upload/v1633172502/Pokemon-tcg-price-guide/home-background.jpg',
+    sectionImgURL:
       'https://res.cloudinary.com/jleomorris/image/upload/v1636328176/Portfolio-v5/Projects/Charizard.png',
   },
   {
+    link: 'Modern_Pokedex',
     subTitle: 'Modern Pokedex',
     title: 'A Modern Twist On The First Gen Pokedex',
     backgroundColor: '#8ea4df',
     textColor: 'text-black',
-    imgURL:
+    headerTextColor: 'text-white',
+    headerImgURL:
+      'https://res.cloudinary.com/jleomorris/image/upload/v1636768639/Portfolio-v5/Projects/modern-pokedex/header_background.jpg',
+    sectionImgURL:
       'https://res.cloudinary.com/jleomorris/image/upload/f_auto,q_auto/v1636324383/Portfolio-v5/Projects/gyarados.png',
   },
   {
+    link: 'Last_Life',
     subTitle: 'Last Life',
     title: 'Get The Latest News On Your Favourite Games',
     backgroundColor: '#1e2438',
     textColor: 'text-white',
-    imgURL:
+    headerTextColor: 'text-white',
+    headerImgURL:
+      'https://res.cloudinary.com/jleomorris/image/upload/v1636772097/Portfolio-v5/Projects/last-life/header-background.jpg',
+    sectionImgURL:
       'https://res.cloudinary.com/jleomorris/image/upload/f_auto,q_auto/v1636325505/Portfolio-v5/Projects/jin_sakai.png',
   },
   {
+    link: 'Bolt_Music_Player',
     subTitle: 'Bolt Music Player',
     title: 'Code to some chill tracks',
     backgroundColor: '#5C6285',
     textColor: 'text-white',
-    imgURL:
+    headerTextColor: 'text-white',
+    headerImgURL:
+      'https://res.cloudinary.com/jleomorris/image/upload/v1636772390/Portfolio-v5/Projects/bolt-music-player/header-background.jpg',
+    sectionImgURL:
       'https://res.cloudinary.com/jleomorris/image/upload/f_auto,q_auto/v1636326862/Portfolio-v5/Projects/bolt.png',
   },
 ];
