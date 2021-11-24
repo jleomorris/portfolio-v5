78392503aa8b4dc116da4a1568e340ad4ebebb3f
jleomorris <james-morris2010@hotmail.co.uk>
2021-11-24 01:45:42 +0000

    Add footer to TabletMobileHeader to avoid moving of hamburger icon

--- components/TabletMobileHeader.js/index.tsx
+++ components/TabletMobileHeader.js/index.tsx
@@ -3,6 +3,7 @@ import { containerAnimation } from '../../animations';
 import { motion } from 'framer-motion';
 // Components
 import Link from 'next/link';
+import Footer from '../Footer';
 
 const listItemVariants = {
   initial: {
@@ -28,42 +29,49 @@ const TabletMobileHeader: React.FC<IProps> = ({
   setIsTabletMobileHeaderOpen,
 }) => {
   return (
-    <div className='absolute top-0 right-0 z-50 flex flex-col items-center justify-center w-screen h-screen bg-main lg:hidden tablet-mobile-header'>
-      <motion.ul
-        variants={containerAnimation}
-        initial='initial'
-        animate='animate'
-        className='space-y-24 text-8xl font-vesterbroPoster'
-      >
-        <motion.li variants={listItemVariants}>
-          <Link href='/about'>
-            <button
-              onClick={() => setIsTabletMobileHeaderOpen((prev) => !prev)}
-            >
-              <a className={`hover:underline pb-1`}>About</a>
-            </button>
-          </Link>
-        </motion.li>
-        <motion.li variants={listItemVariants}>
-          <Link href='/projects'>
-            <button
-              onClick={() => setIsTabletMobileHeaderOpen((prev) => !prev)}
-            >
-              <a className={`hover:underline pb-1`}>Projects</a>
-            </button>
-          </Link>
-        </motion.li>
-        <motion.li variants={listItemVariants}>
-          <Link href='/contact'>
-            <button
-              onClick={() => setIsTabletMobileHeaderOpen((prev) => !prev)}
-            >
-              <a className={`hover:underline pb-1`}>Contact</a>
-            </button>
-          </Link>
-        </motion.li>
-      </motion.ul>
-    </div>
+    <>
+      <div className='absolute top-0 right-0 z-50 flex flex-col items-center justify-center w-screen min-h-screen bg-main lg:hidden tablet-mobile-header'>
+        <div className='flex flex-col items-center justify-between w-full h-screen'>
+          <motion.ul
+            variants={containerAnimation}
+            initial='initial'
+            animate='animate'
+            className='space-y-24 text-8xl font-vesterbroPoster mt-96'
+          >
+            <motion.li variants={listItemVariants}>
+              <Link href='/about'>
+                <button
+                  onClick={() => setIsTabletMobileHeaderOpen((prev) => !prev)}
+                >
+                  <a className={`hover:underline pb-1`}>About</a>
+                </button>
+              </Link>
+            </motion.li>
+            <motion.li variants={listItemVariants}>
+              <Link href='/projects'>
+                <button
+                  onClick={() => setIsTabletMobileHeaderOpen((prev) => !prev)}
+                >
+                  <a className={`hover:underline pb-1`}>Projects</a>
+                </button>
+              </Link>
+            </motion.li>
+            <motion.li variants={listItemVariants}>
+              <Link href='/contact'>
+                <button
+                  onClick={() => setIsTabletMobileHeaderOpen((prev) => !prev)}
+                >
+                  <a className={`hover:underline pb-1`}>Contact</a>
+                </button>
+              </Link>
+            </motion.li>
+          </motion.ul>
+        </div>
+        <div className='w-full'>
+          <Footer />
+        </div>
+      </div>
+    </>
   );
 };
 
