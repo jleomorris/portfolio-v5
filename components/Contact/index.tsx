8ec2d0f264b92c4901b3b7e856b2fed80138ab1a
jleomorris <james-morris2010@hotmail.co.uk>
2021-11-30 02:19:54 +0000

    Add hand animation to Contact

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,16 +1,50 @@
+import { useState } from 'react';
+// Animation
+import { motion } from 'framer-motion';
 // Components
 import PageContainer, { Variant as pageVariant } from '../PageContainer';
 import Button, { Variant as buttonVariant, Size } from '../Button';
 import Link from 'next/link';
+import Image from 'next/image';
+
+// Variants
+const handAnimation = {
+  initial: {
+    y: 350,
+    opacity: 1,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+  exit: {
+    opacity: 1,
+    y: 350,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 const Contact = () => {
+  const [isButtonHoveredOver, setIsButtonHoveredOver] =
+    useState<boolean>(false);
+
   return (
     <PageContainer variant={pageVariant.NORMAL} noFlex={false}>
       <div className='contact'>
         <h1 className='text-center font-vesterbroPoster text-8xl'>
           {`Let's work together`}
         </h1>
-        <div className='h-20 mx-auto mt-10 w-80'>
+        <div
+          className='relative h-20 mx-auto mt-10 w-80'
+          onMouseEnter={() => setIsButtonHoveredOver(true)}
+          onMouseLeave={() => setIsButtonHoveredOver(false)}
+        >
           <Link href='/contact'>
             <Button
               type='button'
@@ -24,6 +58,19 @@ const Contact = () => {
               </a>
             </Button>
           </Link>
+          <motion.div
+            variants={handAnimation}
+            initial='initial'
+            animate={isButtonHoveredOver ? 'animate' : 'exit'}
+            exit='exit'
+            className='absolute -top-16 sm:-top-36 -right-80 sm:-right-160 h-80 sm:h-160 w-80 sm:w-160'
+          >
+            <Image
+              src='https://res.cloudinary.com/jleomorris/image/upload/v1638234858/Portfolio-v5/3D-stripy/hat-man-wrist-3.png'
+              layout='fill'
+              objectFit='contain'
+            />
+          </motion.div>
         </div>
       </div>
     </PageContainer>
